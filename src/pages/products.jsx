ffdce110e9be34accecd149f8058ab19fc6e99dc
nadillaizt <abhinaya3403@gmail.com>
2024-05-03 20:59:33 +0700

    redirect to logout

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -7,6 +7,13 @@ import { getProducts } from "../services/product.services";
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
 
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   useEffect(() => {
     getProducts((data) => {
       setProducts(data);
@@ -16,21 +23,24 @@ const ProductPage = () => {
   return (
     <>
       <div className="flex justify-end h-12 bg-blue-600 sticky top-0 text-white items-center px-10">
-        <Button type="button" classname="ml-5 bg-black">
+        <Button onClick={handleLogout} type="button" classname="ml-5 bg-black">
           Logout
         </Button>
       </div>
 
       <div className="flex flex-wrap justify-center min-h-screen items-center mt-5">
-        {products.length > 0 && products.map((product) => (
-          <CardProduct key={product.id}>
-            <CardProduct.Header image={product.image} />
-            <CardProduct.Body title={product.title}>
-              {product.description.length > 100 ? `${product.description.substring(0, 100)}...` : product.description}
-            </CardProduct.Body>
-            <CardProduct.Footer price={product.price}></CardProduct.Footer>
-          </CardProduct>
-        ))}
+        {products.length > 0 &&
+          products.map((product) => (
+            <CardProduct key={product.id}>
+              <CardProduct.Header image={product.image} />
+              <CardProduct.Body title={product.title}>
+                {product.description.length > 100
+                  ? `${product.description.substring(0, 100)}...`
+                  : product.description}
+              </CardProduct.Body>
+              <CardProduct.Footer price={product.price}></CardProduct.Footer>
+            </CardProduct>
+          ))}
       </div>
     </>
   );
